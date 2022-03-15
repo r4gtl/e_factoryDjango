e38cf337e1f827eb2f176c515bb83409844377b5
r4gtl <ferrari.stefano75@gmail.com>
2022-03-15 21:00:13 +0100

    Initial commit

---
+++ static/js/main.js
@@ -0,0 +1,15 @@
+
+  
+const date = new Date();
+document.querySelector('.year').innerHTML = date.getFullYear();
+
+setTimeout(function() {
+  $('#message').fadeOut('slow');
+}, 3000);
+
+$('select').selectpicker();
+
+
+
+
+
